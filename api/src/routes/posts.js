4fe31c93ac72276876f28a1ec3fbba01dd26da59
Anastasia Bushmakina <bushnastyaa@mail.ru>
2024-03-07 14:09:39 +0300

    Update post router

--- api/src/routes/posts.js
+++ api/src/routes/posts.js
@@ -40,7 +40,7 @@ router.get('/', async (req, res) => {
   try {
     const posts = await PostModel
     .find()
-    .sort({ createdAt: "asc" });
+    .sort({ createdAt: "desc" });
 
     res.status(200).json(posts);
   } catch (err) {
